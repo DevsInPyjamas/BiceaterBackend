0fd06f79e6fa33890fb32f765e9b3eec55131256
Alkesst <alec.augal@gmail.com>
2019-11-06 17:26:28 +0100

    Removed migrations since they was just tests

--- BiceaterAPI/models.py
+++ BiceaterAPI/models.py
@@ -2,7 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-
-class Rest(models.Model):
-    first_name = models.CharField(max_length=30, default='Jose')
-    last_name = models.CharField(max_length=30, default='Martin')
