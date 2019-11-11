3f70e8d7b137ef25014d1113276a3e89b04e39ad
Samuel Jurado Quintana <samueljurado12@gmail.com>
2019-11-11 21:21:57 +0100

    feature/DB_Operations (#4)
    * Created the requirements.txt
    
    * Updated settings.py to use python-dotenv.
    
    * Removed migrations since they was just tests
    
    * First database schema
    
    Added Users and Comments table
    
    * Fixed id of stations to maintain coherence
    
    * Let's see if this works now

--- BiceaterAPI/models.py
+++ BiceaterAPI/models.py
@@ -12,8 +12,8 @@ class User(models.Model):
     ]
 
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(unique=True, max_length=50)
-    name = models.CharField(max_length=50)
+    username = models.CharField(unique=True, max_length=50, default='')
+    name = models.CharField(max_length=50, default='')
     surname = models.CharField(max_length=50, blank=True)
     DoB = models.DateField(null=True)
     image = models.ImageField(null=True)
@@ -28,15 +28,16 @@ class User(models.Model):
 
 class Comment(models.Model):
     comment_id = models.BigAutoField(primary_key=True)
-    text = models.TextField(max_length=140)
+    text = models.TextField(max_length=140, default='')
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         'User',
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE
     )
     answers_to = models.ForeignKey(
         'BiceaterAPI.Comment',
         on_delete=models.CASCADE,
         null=True
     )
-    bike_hire_docking_station_id = models.CharField(max_length=100)
+    bike_hire_docking_station_id = models.CharField(max_length=100,
+                                                    default='0')
