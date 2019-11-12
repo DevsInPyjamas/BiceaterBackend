89cfea7a5d99345f65326f0380088a79bb460a4e
JesusPa98 <jesusparejo98@gmail.com>
2019-11-12 20:40:24 +0100

    Added user queries
    Signed-off-by: JesusPa98 <jesusparejo98@gmail.com>

--- BiceaterAPI/views.py
+++ BiceaterAPI/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+import json
+from django.http import HttpResponseBadRequest
+from BiceaterAPI import models
+
+
+def all_users(request):
+    query_response = models.User.objects.all()
+    dicted_response = [i.to_dict() for i in query_response]
+    return dicted_response
+
+
+def users_by_username(request):
+    user_input = None
+    if request.method == 'GET' and 'id' in request.GET:
+        user_input = request.GET.get("input", '')
+    if user_input:
+        query_response = models.User.objects.get(username=user_input)
+        dicted_response = [i.to_dict() for i in query_response]
+        return dicted_response
+    else:
+        error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
+        return HttpResponseBadRequest(json.dumps(error_str))
