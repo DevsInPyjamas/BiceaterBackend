a785c5cc9ff96253c35b450f4302a24a19a8c5b3
pedrogilena@gmail.com <fairyTail22>
2019-11-15 18:46:40 +0100

    Comment queries 1 added.

--- BiceaterAPI/urls.py
+++ BiceaterAPI/urls.py
@@ -4,7 +4,9 @@ from django.urls import path
 from BiceaterAPI import views
 
 urlpatterns = [
+    url(r'^comments(?P<stop_input>)/$', views.comment_by_stop, name='Stops comments'),
+    url(r'^comments/(?P<comment_id>[0-9]+)/$', views.comment_of_comment, name='Comments of comments'),
     url(r'^users(?P<user_input>\w{0,50})$', views.users_by_username, name='Users by username'),
-    url(r'^users(?P<user_id>[0-9]+)$', views.users_by_id, name='Users by ID'),
-    url(r'^users/$', views.all_users, name='All users')
+    url(r'^users/(?P<user_id>[0-9]+)$', views.users_by_id, name='Users by ID'),
+    url(r'^users/$', views.all_users, name='All users'),
 ]
