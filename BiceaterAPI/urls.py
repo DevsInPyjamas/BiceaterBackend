5897e4bc7481e393080ae50807cdcc4130cbd136
Jesús Parejo Aliaga <jesusparejo98@gmail.com>
2019-11-16 20:53:09 +0100

    Added decorator to convert the query result to a JSON and fixed some problems with URL's (#11)
    Signed-off-by: JesusPa98 <jesusparejo98@gmail.com>

--- BiceaterAPI/urls.py
+++ BiceaterAPI/urls.py
@@ -5,10 +5,10 @@ from BiceaterAPI import views
 
 urlpatterns = [
     url(r'^users/(?P<user_id>[0-9]+)$', views.users_by_id, name='Users by ID'),
-    url(r'^users/$', views.all_users, name='All users'),
-    url(r'^users/(?P<user_input>\w{0,50})/$', views.users_by_username, name='Users by username'),
+    url(r'^users$', views.all_users, name='All users'),
+    url(r'^users(?P<user_input>\w{0,50})/$', views.users_by_username, name='Users by username'),
     url(r'^users/(?P<user_id>[0-9]+)/comments/$', views.comments_by_user_id, name='Comments by user id'),
-    url(r'^comments/$', views.all_comments, name='All comments'),
+    url(r'^comments$', views.all_comments, name='All comments'),
     url(r'^comments(?P<stop_input>)/$', views.comment_by_stop, name='Stops comments'),
     url(r'^comments/(?P<comment_id>[0-9]+)/$', views.comment_of_comment, name='Comments of comments'),
 ]
