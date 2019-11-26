9d0cc6134ee7b2d7e07685f572d31b5fee1424e1
Samuel Jurado Quintana <samueljurado12@gmail.com>
2019-11-26 18:12:07 +0100

    Rama

--- BiceaterAPI/utils.py
+++ BiceaterAPI/utils.py
@@ -1,9 +1,21 @@
 import requests
 import json
 
+from django.http import HttpResponse, HttpResponseBadRequest
+
 
 def datos_abiertos():
     response = requests.get(
         'https://datosabiertos.malaga.eu/recursos/transporte/EMT/EMTocupestacbici/ocupestacbicifiware.json')
     malaga = response.content
     return json.loads(malaga)
+
+
+def check_authorized(user):
+    if not user.is_authenticated:
+        return HttpResponse('Unauthorized', status=401)
+
+
+def throw_bad_request():
+    error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
+    return HttpResponseBadRequest(json.dumps(error_str))
--- BiceaterAPI/views.py
+++ BiceaterAPI/views.py
@@ -4,16 +4,16 @@ from django.contrib.auth.decorators import login_required
 import json
 from .models import *
 from .decorators import returns_json
-from django.http import HttpResponseBadRequest, HttpResponseForbidden, HttpResponse
-from .utils import datos_abiertos
+from django.http import HttpResponseBadRequest, HttpResponse
+from .utils import datos_abiertos, check_authorized, throw_bad_request
 import re
 
+# READ OPERATIONS
 
 @login_required
 @returns_json
 def all_users(request):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     query_response = AppUser.objects.all()
     dicted_response = [i.to_dict() for i in query_response]
     return dicted_response
@@ -22,8 +22,7 @@ def all_users(request):
 @login_required
 @returns_json
 def users_by_username(request, user_input):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     user_input = None
     if request.method == 'GET' and 'user_input' in request.GET:
         user_input = request.GET.get("user_input", '')
@@ -32,29 +31,26 @@ def users_by_username(request, user_input):
         dicted_response = [i.to_dict() for i in query_response]
         return dicted_response
     else:
-        error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
-        return HttpResponseBadRequest(json.dumps(error_str))
+        throw_bad_request()
 
 
 @login_required
 @returns_json
 def users_by_id(request, user_id):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     if user_id:
-        query_response = AppUser.objects.get(user_id=user_id)
+        user = User.objects.get(user_id=user_id)
+        query_response = AppUser.objects.get(user=user)
         dicted_response = [query_response.to_dict()]
         return dicted_response
     else:
-        error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
-        return HttpResponseBadRequest(json.dumps(error_str))
+        throw_bad_request()
 
 
 @login_required
 @returns_json
 def all_comments(request):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     query_response = Comment.objects.all()
     dicted_response = [i.to_dict() for i in query_response]
     return dicted_response
@@ -63,22 +59,19 @@ def all_comments(request):
 @login_required
 @returns_json
 def comments_by_user_id(request, user_id):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     if user_id:
         query_response = Comment.objects.filter(author=User.objects.get(user_id=user_id))
         dicted_response = [i.to_dict() for i in query_response]
         return dicted_response
     else:
-        error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
-        return HttpResponseBadRequest(json.dumps(error_str))
+        throw_bad_request()
 
 
 @login_required
 @returns_json
 def comment_by_stop(request, stop_input):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     stop_input = None
     if request.method == 'GET' and 'stop_input' in request.GET:
         stop_input = request.GET.get("stop_input", '')
@@ -87,29 +80,44 @@ def comment_by_stop(request, stop_input):
         dicted_response = [i.to_dict() for i in query_response]
         return dicted_response
     else:
-        error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
-        return HttpResponseBadRequest(json.dumps(error_str))
+        throw_bad_request()
 
 
 @login_required
 @returns_json
 def comment_of_comment(request, comment_id):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     if comment_id:
         query_response = Comment.objects.filter(comment_id=comment_id)
         dicted_response = [i.to_dict() for i in query_response]
         return dicted_response
     else:
-        error_str = {'error': 'BAD REQUEST:  It is required to receive an argument'}
-        return HttpResponseBadRequest(json.dumps(error_str))
+        throw_bad_request()
 
+# CREATE OPERATIONS
+@login_required
+def create_comment(request):
+    check_authorized(request.user)
+    text = None
+    bike_hire_docking_station_id = None
+    if(request.method == 'POST' and 'text' in request.POST
+            and 'bike_hire_docking_station_id' in request.POST):
+        text = request.POST['text']
+        bike_hire_docking_station_id = request.POST['bike_hire_docking_station_id']
+    if text and bike_hire_docking_station_id:
+        comment = Comment(text=text, author=AppUser.objects.get(user=request.user),
+                          bike_hire_docking_station_id=bike_hire_docking_station_id)
+        comment.save()
+    else:
+        throw_bad_request()
+
+
+# FETCH API DATOS ABIERTOS
 
 @login_required
 @returns_json
 def fetch_stations(request):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     stations_json = datos_abiertos()
     stations = []
     for element in stations_json:
@@ -124,8 +132,7 @@ def fetch_stations(request):
 @login_required
 @returns_json
 def fetch_station(request, station_id):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    check_authorized(request.user)
     stations_json = datos_abiertos()
     output_dict = [element for element in stations_json if element['id'].split(':')[3] == station_id]
     output_dict = output_dict[0]
