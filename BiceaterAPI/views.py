4e7617ce0d5685943b1306fe9b2e5a9dd9c2f6b4
Alkesst <alec.augal@gmail.com>
2019-12-02 18:15:37 +0100

    Redirections done

--- BiceaterAPI/views.py
+++ BiceaterAPI/views.py
@@ -7,8 +7,8 @@ from django.template import RequestContext
 
 from .models import *
 import json
-from .decorators import returns_json
-from .utils import datos_abiertos, check_authorized, throw_bad_request, throw_forbidden
+from .decorators import returns_json, check_authorized
+from .utils import datos_abiertos, throw_bad_request, throw_forbidden
 from haversine import haversine
 from django.http import HttpResponse
 import re
@@ -16,19 +16,17 @@ import re
 
 # READ OPERATIONS
 
-@login_required
+@check_authorized
 @returns_json
 def all_users(request):
-    check_authorized(request.user)
     query_response = AppUser.objects.all()
     dicted_response = [i.to_dict() for i in query_response]
     return dicted_response
 
 
-@login_required
+@check_authorized
 @returns_json
 def users_by_username(request, user_input):
-    check_authorized(request.user)
     user_input = None
     if request.method == 'GET' and 'user_input' in request.GET:
         user_input = request.GET.get("user_input", '')
@@ -41,10 +39,9 @@ def users_by_username(request, user_input):
         throw_bad_request()
 
 
-@login_required
+@check_authorized
 @returns_json
 def users_by_id(request, user_id):
-    check_authorized(request.user)
     if user_id:
         user = User.objects.get(user_id=user_id)
         query_response = AppUser.objects.get(user=user)
@@ -54,19 +51,17 @@ def users_by_id(request, user_id):
         throw_bad_request()
 
 
-@login_required
+@check_authorized
 @returns_json
 def all_comments(request):
-    check_authorized(request.user)
     query_response = Comment.objects.all()
     dicted_response = [i.to_dict() for i in query_response]
     return dicted_response
 
 
-@login_required
+@check_authorized
 @returns_json
 def comments_by_user_id(request, user_id):
-    check_authorized(request.user)
     if user_id:
         user = User.objects.get(user_id=user_id)
         query_response = Comment.objects.filter(author=AppUser.objects.get(user=user))
@@ -75,10 +70,10 @@ def comments_by_user_id(request, user_id):
     else:
         throw_bad_request()
 
-@login_required
+
+@check_authorized
 @returns_json
 def one_comment_by_user_id(request, user_id, comment_id):
-    check_authorized(request.user)
     if user_id and comment_id:
         query_response = Comment.objects.filter(author=AppUser.objects.get(user_id=user_id), comment_id=comment_id)
         dicted_response = [i.to_dict() for i in query_response]
@@ -86,10 +81,10 @@ def one_comment_by_user_id(request, user_id, comment_id):
     else:
         throw_bad_request()
 
-@login_required
+
+@check_authorized
 @returns_json
 def comment_by_stop(request, stop_input):
-    check_authorized(request.user)
     stop_input = None
     if request.method == 'GET' and 'stop_input' in request.GET:
         stop_input = request.GET.get("stop_input", '')
@@ -101,10 +96,9 @@ def comment_by_stop(request, stop_input):
         throw_bad_request()
 
 
-@login_required
+@check_authorized
 @returns_json
 def comment_of_comment(request, comment_id):
-    check_authorized(request.user)
     if comment_id:
         query_response = Comment.objects.filter(comment_id=comment_id)
         dicted_response = [i.to_dict() for i in query_response]
@@ -113,10 +107,9 @@ def comment_of_comment(request, comment_id):
         throw_bad_request()
 
 # CREATE OPERATIONS
-@login_required
 @csrf_exempt
+@check_authorized
 def create_comment(request):
-    check_authorized(request.user)
     body = json.loads(request.body.decode('utf-8'))
     text = None
     bike_hire_docking_station_id = None
@@ -137,10 +130,9 @@ def create_comment(request):
         throw_bad_request()
 
 
-@login_required
 @csrf_exempt
+@check_authorized
 def update_user(request):
-    check_authorized(request.user)
     app_user = AppUser.objects.get_or_create(user=request.user.id)
     body = json.loads(request.body.decode('utf-8'))
     username = None
@@ -178,9 +170,8 @@ def update_user(request):
         throw_bad_request()
 
 
-@login_required
+@check_authorized
 def delete_comment(request):
-    check_authorized(request.user)
     comment_id = None
     if request.method == 'POST' and 'comment_id' in request.POST:
         comment_id = request.POST['comment_id']
@@ -195,18 +186,17 @@ def delete_comment(request):
         throw_bad_request()
 
 
-@login_required
+@check_authorized
 def delete_user(request):
-    check_authorized(request.user)
     request.user.delete()
 
 
 # FETCH API DATOS ABIERTOS
 
-@login_required
+# @check_authorized
+@check_authorized
 @returns_json
 def fetch_stations(request):
-    check_authorized(request.user)
     stations_json = datos_abiertos()
     stations = []
     for element in stations_json:
@@ -218,10 +208,9 @@ def fetch_stations(request):
     return stations
 
 
-@login_required
+@check_authorized
 @returns_json
 def fetch_station(request, station_id):
-    check_authorized(request.user)
     stations_json = datos_abiertos()
     output_dict = [element for element in stations_json if element['id'].split(':')[3] == station_id]
     output_dict = output_dict[0]
@@ -231,8 +220,8 @@ def fetch_station(request, station_id):
     return output_dict
 
 
-@login_required
 @csrf_exempt
+@check_authorized
 @returns_json
 def calculate_best_route(request):
     location = json.loads(request.body)['currentLocation']
