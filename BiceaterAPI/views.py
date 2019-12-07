e3b6fae1470d0f368f8390abda94736c9813e050
Alejandro Garau Madrigal <alec.augal@gmail.com>
2019-12-07 18:01:25 +0100

    removed unusued imports

--- BiceaterAPI/views.py
+++ BiceaterAPI/views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from http import HTTPStatus
 # Create your views here.
 
 from django.core.paginator import Paginator
-from django.template import RequestContext
 
 from .models import *
 import json
